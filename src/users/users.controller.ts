7da274eb525ae47508369a521f8fa2fd2669f001
Pablo Calle <callejupa@gmail.com>
2026-02-09 19:58:32 -0500

    refactor with service injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,92 +1,35 @@
-import { Controller, Get, Post, Delete, Put, Param, Body, ParseUUIDPipe, HttpCode } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
+import { Controller, Get, Post, Delete, Put, Param, Body, ParseUUIDPipe } from '@nestjs/common';
 import type { UUID } from 'node:crypto';
-
-interface User {
-  id: string;
-  name: string;
-  email: string;
-}
+import { UsersService } from './users.service';
+import type { User } from './user.model';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
 
 @Controller('users')
 export class UsersController {
-  private users: User[] = [
-    {
-      id: randomUUID(),
-      name: 'Juan pa',
-      email: 'juanpa@correo.com',
-    },
-    {
-      id: randomUUID(),
-      name: 'Sofia',
-      email: 'sofi@correo.com',
-    },
-    {
-      id: randomUUID(),
-      name: 'Shalom',
-      email: 'shaloma@correo.com',
-    },
-  ];
+  constructor(private usersService: UsersService) {}
 
   @Get()
   getUsers(): User[] {
-    return this.users;
+    return this.usersService.findAll();
   }
 
   @Get(':id')
   findUser(@Param('id', ParseUUIDPipe) id: UUID) {
-    const user = this.users.find((user) => user.id === id);
-    console.log(user, 'user');
-    if (!user) {
-      return {
-        error: 'User not found',
-      };
-    }
-    return user;
+    return this.usersService.getUserById(id);
   }
 
   @Post()
-  @HttpCode(201)
-  createUser(@Body() body: User) {
-    const newUser = {
-      ...body,
-      id: randomUUID(),
-    };
-    this.users.push(newUser);
-    return {
-      message: 'User created',
-      body: newUser,
-    };
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body);
   }
 
   @Delete(':id')
   deleteUser(@Param('id', ParseUUIDPipe) id: UUID) {
-    const position: number = this.users.findIndex((user) => user.id === id);
-    if (position === -1) {
-      return {
-        error: 'User not found',
-      };
-    }
-    this.users.splice(position, 1);
-    return {
-      message: 'User deleted',
-    };
+    return this.usersService.delete(id);
   }
 
   @Put(':id')
-  updateUser(@Param('id', ParseUUIDPipe) id: UUID, @Body() body: Omit<User, 'id'>) {
-    const position: number = this.users.findIndex((user) => user.id === id);
-    if (position === -1) {
-      return {
-        error: 'User not found',
-      };
-    }
-    const currentData = this.users[position];
-    const updateUser = {
-      ...currentData,
-      ...body,
-    };
-    this.users[position] = updateUser;
-    return updateUser;
+  updateUser(@Param('id', ParseUUIDPipe) id: UUID, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './user.model';
+import { randomUUID } from 'node:crypto';
+import type { UUID } from 'node:crypto';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      id: randomUUID(),
+      name: 'Juan pa',
+      email: 'juanpa@correo.com',
+    },
+    {
+      id: randomUUID(),
+      name: 'Sofia',
+      email: 'sofi@correo.com',
+    },
+    {
+      id: randomUUID(),
+      name: 'Shalom',
+      email: 'shaloma@correo.com',
+    },
+  ];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  getUserById(id: string) {
+    const position = this.findOne(id);
+    return this.users[position];
+  }
+
+  create(body: CreateUserDto) {
+    const newUser = {
+      ...body,
+      id: randomUUID(),
+    };
+    this.users.push(newUser);
+    return {
+      message: 'User created',
+      body: newUser,
+    };
+  }
+
+  update(id: UUID, changes: UpdateUserDto) {
+    const position: number = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      return {
+        error: 'User not found',
+      };
+    }
+    const currentData = this.users[position];
+    const updateUser = {
+      ...currentData,
+      ...changes,
+    };
+    this.users[position] = updateUser;
+    return updateUser;
+  }
+
+  delete(id: string) {
+    const position: number = this.findOne(id);
+    this.users.splice(position, 1);
+    return {
+      message: 'User deleted',
+    };
+  }
+
+  private findOne(id: string) {
+    const position = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return position;
+  }
+}
