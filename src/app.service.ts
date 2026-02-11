e0d9d0077049317561b849f6128dad9370bd2eef
Pablo Calle <callejupa@gmail.com>
2026-02-11 12:52:54 -0500

    add env variables to use in the system

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env.model';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService<Env>) {}
   getHealth(): string {
-    return 'Health is good!';
+    const nameDev = this.configService.get('NAME_DEV', { infer: true });
+    return `Health is good! ${nameDev}`;
   }
 }
