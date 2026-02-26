422075ba49cdf604b8f120c3df356e9ebfe68ad6
Pablo Calle <callejupa@gmail.com>
2026-02-26 13:32:35 -0500

    refactor profile entity

--- src/users/entities/profile.entity.ts
+++ src/users/entities/profile.entity.ts
@@ -11,12 +11,20 @@ export class Profile {
   @Column({ type: 'varchar', length: 255, name: 'last_name' })
   lastName: string;
 
-  @Column({ type: 'text', length: 255, name: 'avatar_url' })
+  @Column({ type: 'text', length: 255, nullable: true, name: 'avatar_url' })
   avatarUrl: string;
 
-  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    name: 'created_at',
+  })
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    name: 'updated_at',
+  })
   updatedAt: Date;
 }
